1e26d1007e5b837de42dcab13cd50f0b447b745e
TrueWebMagic <nicholaspiech@gmail.com>
2024-05-01 20:10:53 +0200

    copy section margin top fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { hotjar } from "react-hotjar";
-import { useEffect } from "react";
+import Script from "next/script";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,32 +16,28 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  useEffect(() => {
-    hotjar.initialize({
-      id: 4942822,
-      sv: 6,
-    });
-  }, []);
   return (
     <html lang="en">
-      {/* <head>
+      <head>
         <Script
-          id="HotJarAnalytics"
+          id="tracking-code"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html:
-              `
-                (function(h,o,t,j,a,r){
-                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                  h._hjSettings={hjid:4942822,hjsv:6};
-                  a=o.getElementsByTagName('head')[0];
-                  r=o.createElement('script');r.async=1;
-                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                  a.appendChild(r);
-                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-              `,
-          }} />
-      </head> */}
+            __html: `
+                <script>
+                  (function(h,o,t,j,a,r){
+                      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                      h._hjSettings={hjid:4942822,hjsv:6};
+                      a=o.getElementsByTagName('head')[0];
+                      r=o.createElement('script');r.async=1;
+                      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                      a.appendChild(r);
+                  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+                </script>
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
