71924d4af29d08e7a1fbafd90031852439f12031
TrueWebMagic <nicholaspiech@gmail.com>
2024-05-09 12:45:20 +0200

    phones update

--- src/components/process-section/ProcessSection.tsx
+++ src/components/process-section/ProcessSection.tsx
@@ -38,7 +38,7 @@ export function ProcessSection() {
                                         <video muted loop autoPlay
                                             className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-0 rounded-[25px] w-[191px] h-[385px] object-cover"
                                         >
-                                            <source src="/shots/damian.mp4" type="video/mp4" className="z-0" />
+                                            <source src="/portfolio/pet1.mp4" type="video/mp4" className="z-0" />
                                             Your browser does not support the video tag.
                                         </video>
                                     </div>
