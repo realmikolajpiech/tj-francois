9a612b57fa8311291b96a5cc4252575885925d6b
TrueWebMagic <nicholaspiech@gmail.com>
2024-05-24 16:18:56 +0200

    team page title change

--- src/smm-components/SMMHeroSection.tsx
+++ src/smm-components/SMMHeroSection.tsx
@@ -11,7 +11,7 @@ export function SMMHeroSection() {
                             <h3 className="text-[#8B8B8B]">Go back</h3>
                         </div>
                     </a>
-                    <h1 className="mt-2 xl:mt-4 font-bold text-[12vw] sm:text-[52px] md:text-[41px] lg:text-[50px] xl:text-[60px] leading-[50px]">Social Media Managers</h1>
+                    <h1 className="mt-2 xl:mt-4 font-bold text-[12vw] sm:text-[52px] md:text-[41px] lg:text-[50px] xl:text-[60px] leading-[50px]">Our Team</h1>
                     <h2 className="mt-[28px] max-w-[650px] text-[16px] md:text-[18px] lg:text-[20px] ">Meet our social media managers.</h2>
                 </div>
             </div>
