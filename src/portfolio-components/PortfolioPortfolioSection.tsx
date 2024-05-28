b6c36132df73ff4f9c1acd0b178e549686f016a4
TrueWebMagic <nicholaspiech@gmail.com>
2024-05-28 22:55:40 +0200

    portfolio page visual update

--- src/portfolio-components/PortfolioPortfolioSection.tsx
+++ src/portfolio-components/PortfolioPortfolioSection.tsx
@@ -4,11 +4,11 @@ import Image from "next/image";
 
 export function PortfolioPortfolioSection() {
     return (
-        <div className="flex px-6 md:px-8 lg:px-12 xl:px-20 mt-16 w-full dk:max-w-[1460px]">
+        <div className="flex px-6 md:px-8 lg:px-12 xl:px-20 mt-16 w-full dk:max-w-[1650px] mx-auto">
             <div className="w-full dk:w-fit flex flex-col dk:flex-row dk:flex-wrap justify-between gap-20">
-                <div className="text-center">
+                <div className="text-center w-[45%]">
                     <h1 className="font-bold text-[44px] leading-tight">Beauty</h1>
-                    <div className="g flex gap-10 mt-6 justify-center flex-wrap">
+                    <div className="g flex gap-8 mt-6 justify-center flex-wrap">
                         <div className="w-[200px] h-[406px] overflow-hidden grow-0 shrink-0 flex justify-center">
                             <div>
                                 <div className="relative h-[420px] w-[216px]">
@@ -56,9 +56,9 @@ export function PortfolioPortfolioSection() {
                         </div>
                     </div>
                 </div>
-                <div className="text-center">
+                <div className="text-center w-[45%]">
                     <h1 className="font-bold text-[44px] leading-tight">Pet</h1>
-                    <div className="g flex gap-10 mt-6 justify-center flex-wrap">
+                    <div className="g flex gap-8 mt-6 justify-center flex-wrap">
                         <div className="w-[200px] h-[406px] overflow-hidden grow-0 shrink-0 flex justify-center">
                             <div>
                                 <div className="relative h-[420px] w-[216px]">
@@ -89,11 +89,26 @@ export function PortfolioPortfolioSection() {
                                 </div>
                             </div>
                         </div>
+                        <div className="w-[200px] h-[406px] overflow-hidden grow-0 shrink-0 flex justify-center">
+                            <div>
+                                <div className="relative h-[420px] w-[216px]">
+                                    <Image src="/portfolio/phone-mockup.png" width={250} height={250} alt="Phone"
+                                        className="w-[200px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 pointer-events-none h-[120%]"
+                                    />
+                                    <video muted loop autoPlay
+                                        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-0 rounded-[25px] w-[191px] h-[385px] object-cover"
+                                    >
+                                        <source src="/portfolio/pet2.mp4" type="video/mp4" className="z-0" />
+                                        Your browser does not support the video tag.
+                                    </video>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="text-center justify-center">
+                <div className="text-center justify-center w-[45%]">
                     <h1 className="font-bold text-[44px] leading-tight">Finance & Tech</h1>
-                    <div className="g flex gap-10 mt-6 justify-center flex-wrap">
+                    <div className="g flex gap-8 mt-6 justify-center flex-wrap">
                         <div className="w-[200px] h-[406px] overflow-hidden grow-0 shrink-0 flex justify-center">
                             <div>
                                 <div className="relative h-[420px] w-[216px]">
@@ -111,9 +126,9 @@ export function PortfolioPortfolioSection() {
                         </div>
                     </div>
                 </div>
-                <div className="text-center">
+                <div className="text-center w-[45%]">
                     <h1 className="font-bold text-[44px] leading-tight">Health & Wellness</h1>
-                    <div className="g flex gap-10 mt-6 justify-center flex-wrap">
+                    <div className="g flex gap-8 mt-6 justify-center flex-wrap">
                         <div className="w-[200px] h-[406px] overflow-hidden grow-0 shrink-0 flex justify-center">
                             <div>
                                 <div className="relative h-[420px] w-[216px]">
@@ -161,9 +176,9 @@ export function PortfolioPortfolioSection() {
                         </div>
                     </div>
                 </div>
-                <div className="text-center">
+                <div className="text-center w-[45%]">
                     <h1 className="font-bold text-[44px] leading-tight">Food / Beverage</h1>
-                    <div className="g flex gap-10 mt-6 justify-center flex-wrap">
+                    <div className="g flex gap-8 mt-6 justify-center flex-wrap">
                         <div className="w-[200px] h-[406px] overflow-hidden grow-0 shrink-0 flex justify-center">
                             <div>
                                 <div className="relative h-[420px] w-[216px]">
@@ -211,9 +226,9 @@ export function PortfolioPortfolioSection() {
                         </div>
                     </div>
                 </div>
-                <div className="text-center">
+                <div className="text-center w-[45%]">
                     <h1 className="font-bold text-[44px] leading-tight">Travel</h1>
-                    <div className="g flex gap-10 mt-6 justify-center flex-wrap">
+                    <div className="g flex gap-8 mt-6 justify-center flex-wrap">
                         <div className="w-[200px] h-[406px] overflow-hidden grow-0 shrink-0 flex justify-center">
                             <div>
                                 <div className="relative h-[420px] w-[216px]">
