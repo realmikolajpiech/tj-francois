adbb5449341be95bf85c988f3b69504f7736cea3
TrueWebMagic <nicholaspiech@gmail.com>
2024-05-30 21:31:22 +0200

    phone lacking video fix

--- src/components/portfolio-section/PortfolioSection.tsx
+++ src/components/portfolio-section/PortfolioSection.tsx
@@ -1,25 +1,22 @@
-'use client';
-
-import Image from "next/image";
-import { Suspense, useEffect, useRef } from "react";
+// import { useRef } from "react";
 import Phone from "./Phone";
 
 export function PortfolioSection() {
 
-    const carouselRef = useRef<HTMLDivElement | null>(null);
-    const phoneRef = useRef<HTMLDivElement | null>(null);
+    // const carouselRef = useRef<HTMLDivElement | null>(null);
+    // const phoneRef = useRef<HTMLDivElement | null>(null);
 
-    const stopCarousel = () => {
-        console.log("func")
-        if (carouselRef.current) {
-            carouselRef.current.classList.add('stop-carousel');
-        }
-    }
+    // const stopCarousel = () => {
+    //     console.log("func")
+    //     if (carouselRef.current) {
+    //         carouselRef.current.classList.add('stop-carousel');
+    //     }
+    // }
 
     return (
         <section className="portfolio-section w-[100vw] h-[850px] items-center gap-10 flex overflow-hidden mt-48 mb-16 md:-mt-36 md:-mb-8 lg:-mt-56 xl:-mt-[13rem] lg:pt-[80px] relative">
             <div className="flex overflow-hidden gap-10 relative w-full h-[850px] items-center mt-48 mb-16 md:-mt-36 md:-mb-8 lg:-mt-48 xl:-mt-[0] lg:pt-[0] animate-stop pointer-events-none">
-                <div className='flex scale-[70%] md:scale-100 animate-scroll absolute mt-10 sm:mt-16 xl:mt-24 group' ref={carouselRef}>
+                <div className='flex scale-[70%] md:scale-100 animate-scroll absolute mt-10 sm:mt-16 xl:mt-24 group'>
                     <div className="flex gap-[4.5rem] shrink-0 -translate-x-[11rem]">
                         <Phone imgOrVid="video" video="/creators/betty2.mp4" />
                         <Phone imgOrVid="video" video="/creators/tommi.mp4" />
