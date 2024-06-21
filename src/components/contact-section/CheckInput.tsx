eef79a7bb0d1e8b6149f278240d26251529038df
TrueWebMagic <nicholaspiech@gmail.com>
2024-06-21 12:38:57 +0200

    contact form visual fix

--- src/components/contact-section/CheckInput.tsx
+++ src/components/contact-section/CheckInput.tsx
@@ -11,7 +11,7 @@ export function CheckInput({ id, value, text }: CheckInputProps) {
         <div className="flex gap-3 items-center relative">
             <input type="checkbox" id={id} name='services' value={value} className="rounded-lg h-6 w-6 outline-[#EDDCC4]" />
             <span className="checkbox-container"></span>
-            <label htmlFor={id}>{text}</label>
+            <label htmlFor={id} className="w-full">{text}</label>
         </div>
     )
 }
