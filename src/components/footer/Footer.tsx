eae986f7ac8d624a0446325f080daef339b004c5
TrueWebMagic <nicholaspiech@gmail.com>
2024-07-19 07:44:14 +0200

    meet our crew page deletion

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -8,7 +8,7 @@ export function Footer() {
     const pathname = usePathname();
     const isHome = pathname === "/";
     const isPortfolio = pathname === "/portfolio";
-    const isCrew = pathname === "/meet-our-crew";
+    // const isCrew = pathname === "/meet-our-crew";
 
     return (
         <footer className="text-center pb-3 md:relative md:overflow-hidden">
@@ -26,7 +26,7 @@ export function Footer() {
             <div className="flex gap-8 w-full gap-y-4 flex-wrap justify-center mb-8 font-medium md:w-fit md:absolute md:left-1/2 md:bottom-8 md:-translate-x-1/2">
                 <a className={`mobile-link ${isHome ? 'active' : ''}`} href="/">Home</a>
                 <a className={`mobile-link ${isPortfolio ? 'active' : ''}`} href="/portfolio">Portfolio</a>
-                <a className={`mobile-link ${isCrew ? 'active' : ''}`} href="/meet-our-crew">Meet Our Crew</a>
+                {/* <a className={`mobile-link ${isCrew ? 'active' : ''}`} href="/meet-our-crew">Meet Our Crew</a> */}
                 <a href="/#contact">Contact</a>
             </div>
             <div className="flex gap-10 md:gap-[2.5vw] lg:gap-8 xl:gap-9 w-full justify-center mb-8 items-center md:w-fit md:absolute md:right-8 lg:right-12 xl:right-20 md:bottom-16 md:m-0">
